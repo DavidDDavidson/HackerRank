c3513cabc9c7f863898a6e375e6229356b93e604
DavidDDavidson <d.agu2805@gmail.com>
2015-07-10 13:07:27 -0700

    Finished 2 challenges

---
+++ Data Structures/Linked Lists/Insert a node at the tail of a linked list/InsertNodes.cc
@@ -0,0 +1,30 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* Insert(Node *head,int data)
+{
+    // Complete this method
+    if (head != NULL){
+        struct Node *newHead = head;
+        struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
+        newNode->data = data;
+        newNode->next = NULL;
+        while(head->next != NULL){
+            head = head->next;
+        }
+        head->next = newNode;
+        return newHead;
+    }else{
+        struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
+        newNode->data = data;
+        newNode->next = NULL;
+        return newNode;
+    }
+}
