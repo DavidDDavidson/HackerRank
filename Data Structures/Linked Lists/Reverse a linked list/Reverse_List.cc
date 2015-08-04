07453d5c013f9b8dbb22ebfca1f402f81e02c7d6
DavidDDavidson <D.agu2805@gmail.com>
2015-08-03 20:42:07 -0700

    FInished Reversing a singly Linked List

---
+++ Data Structures/Linked Lists/Reverse a linked list/Reverse_List.cc
@@ -0,0 +1,25 @@
+/*
+  Reverse a linked list and return pointer to the head
+  The input list will have at least one element
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* Reverse(Node *head)
+{
+  // Complete this method
+  Node previous = NULL; // Node reversed in previous iteration
+  Node current = head;  // Node to be reversed in current iteration
+  while(current != NULL)
+  {
+    Node next = current->next;
+    current->next = previous;
+    previous = current;
+    current = next;
+  }
+  head = previous;
+  return head;
+}
