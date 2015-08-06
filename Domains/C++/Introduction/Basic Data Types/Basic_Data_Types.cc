409dd377170d7b572dd9505a59ecf66da9961fe3
DavidDDavidson <d.agu2805@gmail.com>
2015-08-06 16:24:01 -0700

    Domains/Contests, Added Domains/C++
    Added hierarchy to distinguish between contest challenges and domain
    challenges. Also began the C++ category under domains.

---
+++ Domains/C++/Introduction/Basic Data Types/Basic_Data_Types.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int integer = 0;
+    long long_integer = 1;
+    long long long_long_integer = 2;
+    char character = '3';
+    float floating_point = 4.0;
+    double double_integer = 5.0;
+    scanf("%d %ld %lld %c %f %lf", &integer, &long_integer,
+    &long_long_integer, &character, &floating_point, &double_integer);
+    printf("%d\n%ld\n%lld\n%c\n%f\n%lf\n", integer, long_integer,
+    long_long_integer, character, floating_point, double_integer);
+    return 0;
+}
