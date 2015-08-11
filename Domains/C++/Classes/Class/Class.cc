4418a7a2546ea2145632cac695925c7e7a114892
DavidDDavidson <D.agu2805@gmail.com>
2015-08-11 15:17:44 -0700

    Contributed to C++ domain
    Completed "Strings" subdomain
    Completed "Classes" subdomain

---
+++ Domains/C++/Classes/Class/Class.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+/*
+Enter code for class Student here.
+Read statement for specification.
+*/
+
+class Student {
+  private:
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+  public:
+  /* Unlike Java, you cannoth use this. to reference fields within class methods */
+    int get_age(){
+      return age;
+    }
+    void set_age(int this_age){
+      age = this_age;
+    }
+    string get_first_name(){
+      return first_name;
+    }
+    void set_first_name(string name){
+      first_name = name;
+    }
+    string get_last_name(){
+      return last_name;
+    }
+    void set_last_name(string name){
+      last_name = name;
+    }
+    int get_standard(){
+      return standard;
+    }
+    void set_standard(int this_standard){
+      standard = this_standard;
+    }
+    string to_string(){
+      stringstream stream;
+      stream << age << "," << first_name << "," << last_name << "," << standard;
+      string result = stream.str();
+      return result;
+    }
+};
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+
+    cin >> age >> first_name >> last_name >> standard;
+
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+
+    return 0;
+}
---
+++ Domains/C++/Strings/StringStream/StringStream.cc
@@ -0,0 +1,31 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+   // Complete this function
+   vector<int> parsed;
+   stringstream ss(str);
+   char comma;
+   int integer;
+   //pass characters from the stream until end of stream reached
+   while(ss >> integer)
+   {
+     parsed.push_back(integer);
+     if (ss.peek() == ',')    //looks at next character in data stream
+      ss.ignore();  //ignores it if a comma
+   }
+   return parsed;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+
+    return 0;
+}
