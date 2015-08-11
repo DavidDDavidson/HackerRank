e450bb342baaabdb38d4de0384fb3dac54323ed2
DavidDDavidson <D.agu2805@gmail.com>
2015-08-11 03:05:38 -0700

    Contributed to C++ subdomain
    Completed "Introduction"
    Began "Strings" subdomain

---
+++ Domains/C++/Introduction/Pointer/Pointer.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    *a = *a + *b;
+    /*change the value at a (which is outside the scope) as described */
+    *b = abs(*a -*b -*b);
+    /*value of a differs from original value. Thus, subtract the value
+    as necessary and compute the operation as required. Take it's absolute
+    value */
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
---
+++ Domains/C++/Strings/Strings/Strings.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   // Complete the program
+    string a;
+    cin >> a;
+    string b;
+    cin >> b;
+    //printing the size of both strings on line 2 as specified
+    cout << a.size() << " " << b.size() << "\n";
+    //concatinating the two strings and outputting on line 2 as specified
+    string c = a + b;
+    cout << c << "\n";
+    string d;
+    //switching characters for line 3 output as specified
+    for(int n = 0; n < a.size(); n++)
+    {
+      if(n == 0)
+      {
+        d += b[n];
+      }
+      else
+      {
+        d += a[n];
+      }
+    }
+    string e;
+    for(int n = 0; n < b.size(); n++)
+    {
+      if (n == 0)
+      {
+        e += a[n];
+      }
+      else
+      {
+        e += b[n];
+      }
+    }
+    cout << d << " " << e << "\n";
+    return 0;
+}
