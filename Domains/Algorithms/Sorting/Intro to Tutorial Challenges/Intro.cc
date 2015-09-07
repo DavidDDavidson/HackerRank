4f50c87234e9cc782e92ed0726c4a1663f1bd290
DavidDDavidson <D.agu2805@gmail.com>
2015-09-06 23:26:59 -0700

    Began Java
    Completed Java\Introduction\Welcome to Java!
    Completed Java\Introduction\Java Stdin and Stdout 1
    Completed Algorithms\Sorting\Intro to Tutorial Challenges

--- Domains/Algorithms/Sorting/Intro to Tutorial Challenges/Intro.cc
+++ Domains/Algorithms/Sorting/Intro to Tutorial Challenges/Intro.cc
@@ -8,5 +8,14 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int V, n, index;
+    cin >> V >> n;
+    int ar[n];
+    for(int i = 0; i < n; i++)
+    {
+      cin >> ar[i];
+      if (ar[i] == V) index = i;
+    }
+    cout << index;
     return 0;
 }
---
+++ Domains/Java/Introduction/Java Stdin and Stdout 1/Stdin_Stdout.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      for(int i = 0; i < 3; i++)
+      {
+        int a = sc.nextInt();
+        //Complete this line
+        //Complete this line
+
+        System.out.println(a);
+        //Complete this line
+        //Complete this line
+      }
+    }
+}
